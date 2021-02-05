f57290edb61066d2f684235ae1bf2dc517637c27
zoicvil <891038713645Qaz>
2021-02-05 15:12:50 +0300

    Конвертер валюты [#465464]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,18 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("180 rubles are " + Converter.rubleToDollar(180) + " dollar.");
+    }
+}
