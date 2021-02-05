1c195dbe0cbdde18e5931c8a04bea4b882f79301
zoicvil <891038713645Qaz>
2021-02-05 14:06:34 +0300

    Арифметические операции[#465478]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,21 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
+        byte age = 20;
+
+        char gender = 'f';
+
+        short growth = 185;
+
+        int numberBricksBuildHouse = 1586854725;
+
+        long numberBacteria = 678568568;
+
+        double distanceSpaceObjects = 4355555555555555555345.7;
+
+        boolean workPermit = true;
+
+        float fileSize = 25.4f;
         System.out.println("04.02.2021");
     }
 }
