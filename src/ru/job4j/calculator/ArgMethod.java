d2f01629173b66788128656582d52b835508646b
zoicvil <891038713645Qaz>
2021-02-05 14:47:46 +0300

    Аргументы [#465476]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + " " + age + " yers.");
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 333;
+
+        ArgMethod.hello(name, age);
+
+    }
+}
