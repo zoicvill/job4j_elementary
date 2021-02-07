6ca2d40c66acb9b8aab2c82f03345b75c6f67a56
zoicvil <891038713645Qaz>
2021-02-07 16:25:08 +0300

    Создать программу, вычисляющую факториал. [#465531]

---
+++ src/ru/job4j/loop/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calk(int i) {
+        int result = 1;
+            for (int j = 1; j <= i; j++) {
+                result = result * j;
+            }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calk(-5));
+    }
+}
---
+++ test/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int res = 120;
+        int ont = Factorial.calk(5);
+        assertThat(res, is(ont));
+    }
+    @Test
+    public void whenCalculateFactorial() {
+        int res = 3628800;
+        int ont = Factorial.calk(10);
+        assertThat(res, is(ont));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int res = 1;
+        int ont = Factorial.calk(0);
+        assertThat(res, is(ont));
+    }
+}
