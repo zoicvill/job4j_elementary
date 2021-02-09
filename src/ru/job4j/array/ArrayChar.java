863141a6b3e582e173fbc03cc21dfe7aed407de1
zoicvil <891038713645Qaz>
2021-02-09 16:43:55 +0300

    Слово начинается с ... [#465434]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] == pref[i])
+                result = true;
+            else result = false;
+        }
+        return result;
+    }
+}
