7af69f50d4abc5786c0241ea420acd2042101e8d
zoicvil <891038713645Qaz>
2021-02-09 14:00:10 +0300

    Заполнение массива. [#465446]

--- src/ru/job4j/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,15 @@ package ru.job4j.ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println(ages.length);
-        String[] surnames = new String[100500];
-        System.out.println(surnames.length);
-        float[] prices = new float[40];
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Petr Arsentev1";
+        names[2] = "Petr Arsentev2";
+        names[3] = "Petr Arsentev3";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
