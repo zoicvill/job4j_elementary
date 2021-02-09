695c70a7ab6362f94c0c622aa5ef51acb44161b8
zoicvil <891038713645Qaz>
2021-02-09 14:37:52 +0300

    Массивы и цикл for-each [#465456]

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -3,9 +3,11 @@ package ru.job4j.array;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] i = new int[5];
-        for (int j = 0; j <= i.length-1; j++) {
+        for (int j = 0; j < i.length; j++) {
             i[j] = j * 2 + 3;
-            System.out.println(i[j]);
+        }
+        for (int s : i) {
+            System.out.println(s);
         }
     }
 }
