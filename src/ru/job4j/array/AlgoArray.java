4083f0af6670064aca8f533bf23da8b2eb1ba1e8
zoicvil <891038713645Qaz>
2021-02-09 15:33:58 +0300

    Упорядочить массив. [#465462]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 3, 2, 1, 4};
+
+        int oneToFive = array[0];
+        array[0] = array[3];
+        array[3] = oneToFive;
+
+        int twoToThree = array[1];
+        array[1] = array[2];
+        array[2] = twoToThree;
+
+        int fiveToFour = array[3];
+        array[3] = array[4];
+        array[4] = fiveToFour;
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
