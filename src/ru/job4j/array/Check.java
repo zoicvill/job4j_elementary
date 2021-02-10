cb4365b164153da8e57224f91aec9eb5201a13a4
zoicvil <891038713645Qaz>
2021-02-10 13:38:14 +0300

    Массив заполнен true или false [#465435]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,9 +4,10 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            if (result == data[i])
-                result = true;
-            else result = false;
+            if (data[i] != data[0]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
