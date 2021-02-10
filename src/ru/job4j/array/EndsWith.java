72a03b222ceb058a936277afd422faeb3e4fcd2f
zoicvil <891038713645Qaz>
2021-02-10 13:49:41 +0300

    Слово заканчивается на ... [#465438]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean b = true;
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+                b = false;
+                break;
+            }
+
+        }
+        return b;
+    }
+
+}
